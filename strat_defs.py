76a282c1375224d47d67c6591483b67e336bbe59
Kyle Zengo <zengokp@gmail.com>
2024-12-21 15:52:47 -0500

    proba

--- strat_defs.py
+++ strat_defs.py
@@ -3,8 +3,6 @@ import numpy as np
 from prophet import Prophet
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.linear_model import LogisticRegression
-# from sklearn.metrics import accuracy_score, classification_report, confusion_matrix
-# from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelEncoder, StandardScaler
 from xgboost import XGBClassifier
 
@@ -134,14 +132,14 @@ def backtest_strategy(data, ticker, initial_capital, strategy, target, **kwargs)
         data.loc[data[target+"_"+ticker] > data['VWAP'], 'Signal'] = -1  # Sell above VWAP
 
     elif strategy == 'Bollinger':
-        window = kwargs.get('bollinger_window', 20)
-        num_std = kwargs.get('num_std', 2)
-        data['Moving_Avg'] = data[target+"_"+ticker].rolling(window=window).mean()
-        data['Std_Dev'] = data[target+"_"+ticker].rolling(window=window).std()
-        data['Upper_Band'] = data['Moving_Avg'] + (data['Std_Dev'] * num_std)
-        data['Lower_Band'] = data['Moving_Avg'] - (data['Std_Dev'] * num_std)
-        data['Signal'] = 0
-        data.loc[data[target+"_"+ticker] < data['Lower_Band'], 'Signal'] = 1  # Buy
+        bollinger_window = kwargs.get('bollinger_window')
+        bollinger_num_std = kwargs.get('bollinger_num_std')
+        data['Moving_Avg'] = data[target+"_"+ticker].rolling(window=bollinger_window).mean()
+        data['Std_Dev'] = data[target+"_"+ticker].rolling(window=bollinger_window).std()
+        data['Upper_Band'] = data['Moving_Avg'] + (data['Std_Dev'] * bollinger_num_std)
+        data['Lower_Band'] = data['Moving_Avg'] - (data['Std_Dev'] * bollinger_num_std)
+        data['Signal'] = 1 # was 0
+        # data.loc[data[target+"_"+ticker] < data['Lower_Band'], 'Signal'] = 1  # Buy
         data.loc[data[target+"_"+ticker] > data['Upper_Band'], 'Signal'] = -1  # Sell
  
     elif strategy == 'Breakout':
@@ -260,6 +258,56 @@ def backtest_strategy(data, ticker, initial_capital, strategy, target, **kwargs)
             X_test = X.iloc[i:prediction_end]
             data.loc[data.index[i:prediction_end], 'Signal'] = model.predict(X_test)
 
+    elif strategy == "XGBoost_scaled":
+        initial_training_period = kwargs.get('initial_training_period')
+        retrain_interval = kwargs.get('retrain_interval')
+        selected_features = kwargs.get('selected_features')
+
+        data = calculate_technical_indicators(data, ticker, target)
+    
+        # Define target variable: price direction (1 = up, -1 = down, 0 = stable)
+        data['Target'] = np.sign(data[target+"_"+ticker].shift(-1) - data[target+"_"+ticker])
+        
+        # Drop rows with missing values due to rolling calculations
+        data = data.dropna()
+        
+        # Define features and target
+        X = data[selected_features]
+        y = data['Target']
+        
+        # model_params = model_params or {'eval_metric': 'logloss', 'random_state': 42}  # add option for model_params?
+        model_params = {'eval_metric': 'logloss', 'random_state': 42}                    # add option for model_params?
+        model = XGBClassifier(**model_params)                                            # add option for model_params?
+        le = LabelEncoder()
+        scaler = StandardScaler()
+        
+        # Prepare columns
+        data['Signal'] = 1
+
+        for i in range(initial_training_period, len(data), retrain_interval):
+            # Train only on past data up to the current point
+            train_data = data.iloc[:i]
+            X_train = train_data[selected_features]
+            y_train = le.fit_transform( train_data['Target'] )
+
+            # Fit the scaler on the training data
+            scaler.fit(X_train)
+
+            # Scale training data and fit model
+            X_train_scaled = scaler.transform(X_train)
+
+            model.fit(X_train_scaled, y_train)
+
+            # Predict for the next retrain_interval days
+            prediction_end = min(i + retrain_interval, len(data))
+            test_data = data.iloc[i:prediction_end]
+            X_test = test_data[selected_features]
+
+            # Scale test data using already fitted scaler
+            X_test_scaled = scaler.transform(X_test)
+
+            data.loc[data.index[i:prediction_end], 'Signal'] = le.inverse_transform(model.predict(X_test_scaled))
+
     elif strategy == "XGBoost":
         initial_training_period = kwargs.get('initial_training_period')
         retrain_interval = kwargs.get('retrain_interval')
@@ -298,7 +346,21 @@ def backtest_strategy(data, ticker, initial_capital, strategy, target, **kwargs)
             prediction_end = min(i + retrain_interval, len(data))
             test_data = data.iloc[i:prediction_end]
             X_test = test_data[selected_features]
-            data.loc[data.index[i:prediction_end], 'Signal'] = le.inverse_transform(model.predict(X_test))
+
+            # Get predictions and probabilities
+            predicted_classes = model.predict(X_test)
+            predicted_probabilities = model.predict_proba(X_test)
+
+            data.loc[data.index[i:prediction_end], 'Signal'] = le.inverse_transform(predicted_classes)
+
+            # proba
+            # data.loc[data.index[i:prediction_end], 'Signal_proba'] = predicted_probabilities
+
+            # store the probabilities for each class in separate columns
+            for class_index, class_name in enumerate(le.classes_):
+                probability_column = f"proba_Signal{class_name}"
+                data.loc[data.index[i:prediction_end], probability_column] = predicted_probabilities[:, class_index]
+            
     
     elif strategy == 'Perfection':
         data['Signal'] = 1
