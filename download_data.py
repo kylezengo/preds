0725a44865f8f4ed3448cf5fcf66f1998ab56723
kyle-SWSK <zengokp@gmail.com>
2025-01-21 12:57:54 -0500

    linted

--- download_data.py
+++ download_data.py
@@ -1,10 +1,13 @@
+"""This module downloads the data necessary to test different strategies"""
+
 import os
-import pandas as pd
-import requests
 import time
 import urllib.parse
-import yfinance as yf
 from datetime import datetime, date, timedelta
+
+import pandas as pd
+import requests
+import yfinance as yf
 from dotenv import load_dotenv
 from fredapi import Fred
 
@@ -17,12 +20,12 @@ start_date = "1993-01-29" # SPY launched on 1993-01-22 ... first data is January
 end_date = datetime.today().strftime('%Y-%m-%d')
 
 # Get list of S&P 500 tickers from Wikipedia page
-sp_wiki = requests.get("https://en.wikipedia.org/wiki/List_of_S%26P_500_companies")
+sp_wiki = requests.get("https://en.wikipedia.org/wiki/List_of_S%26P_500_companies", timeout=20)
 
 sp_df = pd.read_html(sp_wiki.content)[0]
 
 # Get the wikipedia page for each company (part of url)
-sp_df['wiki_page'] = sp_df['Security'].apply(lambda x: urllib.parse.quote_plus(x))
+sp_df['wiki_page'] = sp_df['Security'].apply(urllib.parse.quote_plus)
 
 sp_df.loc[sp_df['wiki_page']=="Alphabet+Inc.+%28Class+A%29",'wiki_page'] = "Alphabet_Inc."
 sp_df.loc[sp_df['wiki_page']=="Alphabet+Inc.+%28Class+C%29",'wiki_page'] = "Alphabet_Inc."
@@ -57,18 +60,30 @@ missing=[]
 for page in set(sp_df['wiki_page']):
     url = f"https://wikimedia.org/api/rest_v1/metrics/pageviews/per-article/en.wikipedia/all-access/user/{page}/daily/{sdate}/{edate}"
 
-    response = requests.get(url,headers=headers)
     try:
-        df = pd.DataFrame(response.json()['items'])
-        df['ticker'] = sp_df.loc[sp_df['wiki_page']==page,'Symbol'].item()
-    except:
-        print(f"wiki pageviews error: {page}")
+        response = requests.get(url, headers=headers, timeout=20)
+        response.raise_for_status()
+
+        json_data = response.json()
+        if 'items' in json_data:
+            df = pd.DataFrame(json_data['items'])
+            df['ticker'] = sp_df.loc[sp_df['wiki_page']==page,'Symbol'].item()
+        else:
+            print(f"'items' key missing in response for page: {page}")
+            missing.append(page)
+
+    except requests.exceptions.RequestException as e:
+        print(f"Request error for {page}: {e}")
         missing.append(page)
-    
+
+    except ValueError as e:
+        print(f"ValueError for page {page}: {e}")
+        missing.append(page)
+
     if len(df)==0:
         print(f"wiki pageviews df empty: {page}")
         missing.append(page)
-    
+
     dat.append(df)
 
 wiki_pageviews = pd.concat(dat).reset_index(drop=True)
@@ -106,7 +121,7 @@ for i in sp500_tickers:
         df = pd.DataFrame(df)
         df['ticker'] = i
         dat_list.append(df)
-        
+
 os_df = pd.concat(dat_list)
 
 os_df = os_df.rename_axis('os_report_datetime').reset_index()
@@ -128,23 +143,23 @@ for index, row in os_df_date_tick.iterrows():
     current_date = row['os_report_date']
     current_ticker = row['ticker']
     current_value = row['outstanding_shares']
-    
+
     if prev_date is not None:
         # Generate missing dates and values
         missing_dates = pd.date_range(start=prev_date, end=current_date, inclusive='neither')
-        
+
         # Append missing dates and values to dat_list
         dat_list.append(pd.DataFrame({'os_report_date': prev_date
                                     ,'date': missing_dates
                                     ,'ticker': prev_ticker
                                     ,'outstanding_shares': prev_value}))
-    
+
     # Add the current row to dat_list
     dat_list.append(pd.DataFrame({'os_report_date': [current_date]
                                 ,'date': [current_date]
                                 ,'ticker': [current_ticker]
                                 ,'outstanding_shares': [current_value]}))
-    
+
     # Update previous date and value
     prev_date = current_date
     prev_ticker = current_ticker
@@ -153,15 +168,15 @@ for index, row in os_df_date_tick.iterrows():
 os_df_days = pd.concat(dat_list, ignore_index=True)
 
 
-# # Federal funds rate (not exact match to current numbers?)
-# fred = Fred(api_key=fred_api_key)
+# Federal funds rate (not exact match to current numbers?)
+fred = Fred(api_key=fred_api_key)
 
-# ffr = fred.get_series('FEDFUNDS').to_frame(name='federal_funds_rate')
+ffr = fred.get_series('FEDFUNDS').to_frame(name='federal_funds_rate')
 
-# ffr.loc[datetime.today().strftime('%Y-%m-%d'), 'federal_funds_rate'] = ffr['federal_funds_rate'][-1]
+ffr.loc[datetime.today().strftime('%Y-%m-%d'), 'federal_funds_rate'] = ffr['federal_funds_rate'][-1]
 
-# ffr = ffr.resample('D').ffill()
-# ffr = ffr.reset_index(names='Date')
+ffr = ffr.resample('D').ffill()
+ffr = ffr.reset_index(names='Date')
 
 
 # Get historical weaher data for NYC from NOAA
@@ -171,13 +186,13 @@ end_date = datetime.strptime(end_date, "%Y-%m-%d")
 date_ranges = []
 while current_start_date <= end_date:
     current_end_date = current_start_date + timedelta(days=29)
-    
+
     if current_end_date > end_date:
         current_end_date = end_date
-    
+
     date_ranges.append({'start_date': current_start_date.strftime("%Y-%m-%d")
                         ,'end_date': current_end_date.strftime("%Y-%m-%d")})
-    
+
     current_start_date = current_end_date + timedelta(days=1)
 
 date_ranges_df = pd.DataFrame(date_ranges)
@@ -198,21 +213,19 @@ for index, row in date_ranges_df.iterrows():
         'units': 'metric',  # Use metric units (Celsius for temperatures, mm for precipitation)
         'limit': 1000  # Maximum number of records to fetch
     }
-    
-    response = requests.get(noaa_base_url, headers=headers, params=params)
-    
-    response = requests.get(noaa_base_url, headers=headers, params=params)
-    
+
+    response = requests.get(noaa_base_url, headers=headers, params=params, timeout=20)
+
     if response.status_code != 200:
         print(f'Error at start date {row['start_date']}: {response.status_code}, {response.text}')
         print(f'Trying start date {row['start_date']} again')
         time.sleep(10)
-        response = requests.get(noaa_base_url, headers=headers, params=params)
+        response = requests.get(noaa_base_url, headers=headers, params=params, timeout=20)
         if response.status_code != 200:
             print(f'Error at start date {row['start_date']}: {response.status_code}, {response.text}')
-            print(f'Failed twice. Not trying again') 
+            print("Failed twice. Not trying again")
             break
-    
+
     print(response.status_code, row['start_date'])
     data = response.json()
 
@@ -220,7 +233,7 @@ for index, row in date_ranges_df.iterrows():
         date = item['date']
         datatype = item['datatype']
         value = item['value']
-        
+
         # Initialize date entry if not already present
         if date not in weather_data:
             weather_data[date] = {'date': date
@@ -228,7 +241,7 @@ for index, row in date_ranges_df.iterrows():
                                     ,'low_temp_nyc': None
                                     ,'precipitation_PRCP_nyc': None
                                     ,'precipitation_SNOW_nyc': None}
-    
+
         # Update the weather data dictionary based on the datatype
         if datatype == 'TMAX':
             weather_data[date]['high_temp_nyc'] = value
@@ -251,5 +264,5 @@ sp_df.to_csv(f'sp_df_{datetime.today().strftime("%Y%m%d")}.csv', index=False)
 wiki_pageviews.to_csv(f'wiki_pageviews_{datetime.today().strftime("%Y%m%d")}.csv', index=False)
 stocks_df.to_csv(f'stocks_df_{datetime.today().strftime("%Y%m%d")}.csv', index=False)
 os_df_days.to_csv(f'os_df_days_{datetime.today().strftime("%Y%m%d")}.csv', index=False)
-# ffr.to_csv(f'ffr_{datetime.today().strftime("%Y%m%d")}.csv', index=False)
+ffr.to_csv(f'ffr_{datetime.today().strftime("%Y%m%d")}.csv', index=False)
 weather_df.to_csv(f'weather_df_{datetime.today().strftime("%Y%m%d")}.csv', index=False)
