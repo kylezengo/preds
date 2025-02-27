c382b045a6b0f653acaca91ece87cb3e928c2070
Kyle Zengo <zengokp@gmail.com>
2025-02-27 10:20:17 -0500

    refactor: update ticker symbols and enhance strategy list in analysis notebooks

--- strat_defs.py
+++ strat_defs.py
@@ -29,17 +29,25 @@ class KerasConfig:
     epochs: int = 20
 
 @dataclass
+class ProbaConfig:
+    """
+    Minimum probabilities for Signal = 1
+    """
+    # keras: float = 0.5
+    knn: float = 0.5
+    logit: float = 0.5
+    mlp: float = 0.5
+    rf: float = 0.5
+    svc: float = 0.5
+    xgboost: float = 0.5
+
+@dataclass
 class BacktestConfig:
     """
     Backtest configuration class
     """
     overbought: int = 70
-    knn_proba: float = 0.5
-    logit_proba: float = 0.5
-    mlp_proba: float = 0.5
-    rf_proba: float = 0.5
-    svc_proba: float = 0.5
-    xgboost_proba: float = 0.5
+    proba: ProbaConfig = field(default_factory=ProbaConfig)
     keras: KerasConfig = field(default_factory=KerasConfig)
 
 # helper functions
@@ -433,18 +441,15 @@ def strat_svc_proba(data, initial_train_period, svc_proba, random_state=None, n_
     Parameters:
         data (DataFrame): Stock data with required columns.
         initial_train_period (int): Initial training period.
+        svc_proba (float): Probability threshold for Signal = 1.
         random_state (int, optional): Random state for reproducibility.
+        n_jobs (int, optional): Number of parallel jobs for GridSearchCV.
     
     Returns:
         DataFrame: Data with strategy signals.
         model: Trained SVC probability model.
         score: Model accuracy score.
     """
-    pipeline = make_pipeline(
-        StandardScaler(),
-        SVC(probability=True, random_state=random_state)
-    )
-
     feats = [col for col in data.columns if col not in ['Date', 'Target']]
 
     # Drop rows with missing values due to rolling calculations
@@ -477,19 +482,29 @@ def strat_svc_proba(data, initial_train_period, svc_proba, random_state=None, n_
 def strat_keras(data, initial_train_period, config: KerasConfig, random_state=None):
     """
     Predict with Keras
-    """
-    scaler = StandardScaler()
 
-    selected_features = [x for x in list(data) if x not in ['Date','Target']]
+    Parameters:
+        data (DataFrame): Stock data with required columns.
+        initial_train_period (int): Initial training period.
+        config: KerasConfig
+        random_state (int, optional): Random state for reproducibility.
+
+    Returns:
+        DataFrame: Data with strategy signals.
+        model: Trained Keras model.
+    """
+    feats = [col for col in data.columns if col not in ['Date', 'Target']]
 
     # Drop rows with missing values due to rolling calculations
     data = data.dropna().copy()
+
+    scaler = StandardScaler()
 
     tf.random.set_seed(random_state) # seems like the seed is very influential...
     sequence_length = config.sequence_length  # Number of time steps (lookback window)
 
     model = models.Sequential([
-        layers.Input(shape=(sequence_length, len(selected_features))),
+        layers.Input(shape=(sequence_length, len(feats))),
         layers.LSTM(32, activation='relu', dropout=0.2, recurrent_dropout=0.2),
         layers.Dense(16, activation='relu'),
         layers.Dense(1, activation='sigmoid') # Sigmoid for binary classification
@@ -497,17 +512,15 @@ def strat_keras(data, initial_train_period, config: KerasConfig, random_state=No
 
     model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
 
+    proba_results = []
     for i in range(initial_train_period, len(data)):
         # Train only on past data up to the current point
         train_data = data.iloc[:i]
-        X_train_0 = train_data[selected_features]
+        X_train_0 = train_data[feats]
         y_train_0 = train_data['Target']
 
-        # Fit the scaler on the training data
-        scaler.fit(X_train_0)
-
-        # Scale training data and fit model
-        X_train_0_scaled = scaler.transform(X_train_0)
+        # Fit the scaler on the training data and scale training data
+        X_train_0_scaled = scaler.fit_transform(X_train_0)
 
         # Select features and target
         X = []
@@ -530,17 +543,24 @@ def strat_keras(data, initial_train_period, config: KerasConfig, random_state=No
 
         # Predict the next day - use the last sequence_length rows of all features as input
         last_sequence = X_train_0_scaled[-sequence_length:, :].reshape(1, sequence_length,
-                                                                       len(selected_features))
+                                                                       len(feats))
 
         # Make the prediction
         next_day_prediction = model.predict(last_sequence, verbose=0)[0][0]
-        sig_check = 0 if next_day_prediction < config.proba else 1
-        print(f"sig: {sig_check}; "
-              f"Date: {max(train_data['Date']).strftime('%Y-%m-%d')}; "
-              f"next_day_pred: {next_day_prediction} "
-              f"Percent 1: {sum(y_train_0[-sequence_length:])/sequence_length:.0%}")
 
-        data.loc[data.index[i], 'next_day_prediction'] = next_day_prediction
+        # Store predictions with indices
+        proba_results.append((i, next_day_prediction))
+
+        print(f"Date: {max(train_data['Date']).strftime('%Y-%m-%d')}; "
+              f"next_day_pred: {next_day_prediction} "
+              f"Sequence Percent 1: {sum(y_train_0[-sequence_length:])/sequence_length:.0%}")
+
+        # data.loc[data.index[i], 'next_day_prediction'] = next_day_prediction
+
+    proba_df = pd.DataFrame(proba_results,
+                            columns=["index", "next_day_prediction"]).set_index("index")
+    data["next_day_prediction"] = pd.DataFrame(proba_df["next_day_prediction"].to_list(),
+                                               index=proba_df.index)
 
     data['Signal'] = np.where(data['next_day_prediction'].fillna(1) > config.proba, 1, 0)
 
@@ -681,19 +701,19 @@ def backtest_strategy(data, strategy, target, ticker, config: BacktestConfig, ra
     elif strategy == "Logit":
         initial_train_period = kwargs.get('initial_train_period')
         n_jobs = kwargs.get('n_jobs')
-        data, model, score = strat_logit(data, initial_train_period, config.logit_proba,
+        data, model, score = strat_logit(data, initial_train_period, config.proba.logit,
                                          n_jobs=n_jobs)
 
     elif strategy == "RandomForest":
         initial_train_period = kwargs.get('initial_train_period')
         n_jobs = kwargs.get('n_jobs')
-        data, model, score = strat_random_forest(data, initial_train_period, config.rf_proba,
+        data, model, score = strat_random_forest(data, initial_train_period, config.proba.rf,
                                                  random_state, n_jobs)
 
     elif strategy == "KNN":
         initial_train_period = kwargs.get('initial_train_period')
         n_jobs = kwargs.get('n_jobs')
-        data, model, score = strat_knn(data, initial_train_period, config.knn_proba, n_jobs)
+        data, model, score = strat_knn(data, initial_train_period, config.proba.knn, n_jobs)
 
     elif strategy == "GradientBoosting":
         initial_train_period = kwargs.get('initial_train_period')
@@ -702,7 +722,7 @@ def backtest_strategy(data, strategy, target, ticker, config: BacktestConfig, ra
     elif strategy == "XGBoost":
         initial_train_period = kwargs.get('initial_train_period')
         n_jobs = kwargs.get('n_jobs')
-        data, model, score = strat_xgboost(data, initial_train_period,config.xgboost_proba,
+        data, model, score = strat_xgboost(data, initial_train_period,config.proba.xgboost,
                                            random_state, n_jobs)
 
     elif strategy == "SVC":
@@ -711,7 +731,7 @@ def backtest_strategy(data, strategy, target, ticker, config: BacktestConfig, ra
 
     elif strategy == "SVC_proba":
         initial_train_period = kwargs.get('initial_train_period')
-        data, model, score = strat_svc_proba(data, initial_train_period, config.svc_proba,
+        data, model, score = strat_svc_proba(data, initial_train_period, config.proba.svc,
                                              random_state)
 
     elif strategy == "LinearSVC":
@@ -721,16 +741,13 @@ def backtest_strategy(data, strategy, target, ticker, config: BacktestConfig, ra
     elif strategy == "MLP":
         initial_train_period = kwargs.get('initial_train_period')
         n_jobs = kwargs.get('n_jobs')
-        data, model, score = strat_mlp(data, initial_train_period, config.mlp_proba,
+        data, model, score = strat_mlp(data, initial_train_period, config.proba.mlp,
                                        random_state=random_state, n_jobs=n_jobs)
 
     elif strategy == "Keras":
         initial_train_period = kwargs.get('initial_train_period')
         data, model = strat_keras(data, initial_train_period, config=config.keras,
                                   random_state=random_state)
-
-    elif strategy == 'Model of Models':
-        pass # WIP
 
     elif strategy == "Perfection":
         data['Signal'] = 1
